811ae1082579348ca6e3da8eeb8aac437c21f9c5
Joshua Galvez <joshuagalvez119@gmail.com>
2021-05-25 13:39:43 -0700

    added results array

--- js/script.js
+++ js/script.js
@@ -17,10 +17,11 @@ searchBtn.addEventListener("click", function (){
     window.location.replace('./search-results.html');
 })
 
-//var url = 'https://www.loc.gov/search?fo=json&q=los-angeles';
+var url = 'https://www.loc.gov/search?fo=json&q=los-angeles';
 var baseUrl = 'https://www.loc.gov/search?fo=json';
 var urlParmaeter = '&q=los-angeles';
 
+var searchResults= [];
 var dataTitle;
 var dataDate;
 var dataSubject;
@@ -38,21 +39,22 @@ fetch(baseUrl, {
     .then(function (data) {
         console.log(data);
         for(var i = 0; i < data.results.length; i++){
-            console.log(data.results[i].title);
-            console.log(data.results[i].date);
-            console.log(data.results[i].subject);
-            console.log(data.results[i].description);
-            console.log(data.results[i].url);
-            console.log('---------------------------------------------');
+            //console.log(data.results[i].title);
+            //console.log(data.results[i].date);
+            //console.log(data.results[i].subject);
+            //console.log(data.results[i].description);
+            //console.log(data.results[i].url);
+            //console.log('---------------------------------------------');
 
-            dataTitle = data.results[i].title;
-            dataDate = data.results[i].date;
-            dataSubject = (data.results[i].subject);
-            dataDescription = (data.results[i].description);
-            dataUrl = data.results[i].url;
+            var searchResult = {
+                dataTitle: data.results[i].title,
+                dataDate: data.results[i].date,
+                dataSubject: (data.results[i].subject),
+                dataDescription: (data.results[i].description[0]),
+                dataUrl: data.results[i].url,
+            }
+            searchResults.push(searchResult);
         }
+        console.log(searchResults);
         
     });
-      
-
-    
